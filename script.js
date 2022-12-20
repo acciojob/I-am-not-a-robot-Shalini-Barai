c35f998f112cc7330a07cffc3c49638326cc5faf
Shalini-Barai <115135802+Shalini-Barai@users.noreply.github.com>
2022-12-20 13:39:03 +0530

    robot

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-//your code here
+let img1=document.getElementById("img1");
+let img2=document.getElementById("img2");
+let img3=document.getElementById("img3");
+let img4=document.getElementById("img4");
+let img5=document.getElementById("img5");
